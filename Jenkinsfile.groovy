03b0244e7cbc8463b82873d4d85aae7332552f41
Jin <jbjxu@thoughtworks.com>
2020-01-10 10:28:13 +0800

    chore: update jenkins file (use absolute path for cargo)

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh "cargo build"
+                sh "/home/ubuntu/.cargo/bin/cargo build"
             }
         }
         stage("Move libplugin_sample.so file to test folder") {
@@ -14,23 +14,23 @@ pipeline {
         }
         stage('Test') {
             steps {
-                sh "cargo test"
+                sh "/home/ubuntu/.cargo/bin/cargo test"
             }
         }
         stage('Clippy') {
             steps {
-                sh "cargo +nightly clippy --all"
+                sh "/home/ubuntu/.cargo/bin/cargo +nightly clippy --all"
             }
         }
         stage('Rustfmt') {
             steps {
                 // The build will fail if rustfmt thinks any changes are required.
-                sh "cargo +nightly fmt --all"
+                sh "/home/ubuntu/.cargo/bin/cargo +nightly fmt --all"
             }
         }
         stage('Doc') {
             steps {
-                sh "cargo doc"
+                sh "/home/ubuntu/.cargo/bin/cargo doc"
                 // We run a python `SimpleHTTPServer` against
                 // /var/lib/jenkins/jobs/<repo>/branches/master/javadoc to display our docs
                 step([$class    : 'JavadocArchiver',
