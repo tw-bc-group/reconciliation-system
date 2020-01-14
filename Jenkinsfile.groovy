fcecad1886211b5a4ea89c14ef2285afe058397e
cheng zhang <cheng.zhang@thoughtworks.com>
2020-01-14 12:42:47 +0800

    set rusmtfmt stage as first stage

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,12 @@ pipeline {
             image 'rust:latest'
         }
     }
-
+    stage('Rustfmt') {
+        steps {
+            // The build will fail if rustfmt thinks any changes are required.
+            sh "rustup component add rustfmt --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo fmt --all"
+        }
+    }
     stages {
         stage('Build') {
             steps {
@@ -24,12 +29,6 @@ pipeline {
         stage('Clippy') {
             steps {
                 sh "rustup component add clippy --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo clippy --all"
-            }
-        }
-        stage('Rustfmt') {
-            steps {
-                // The build will fail if rustfmt thinks any changes are required.
-                sh "rustup component add rustfmt --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo fmt --all"
             }
         }
     }
