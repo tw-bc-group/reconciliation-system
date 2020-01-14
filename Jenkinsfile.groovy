fa212b5d08ae8be715490efbf2628243fb8129ca
cheng zhang <cheng.zhang@thoughtworks.com>
2020-01-14 14:20:45 +0800

    fix ci failed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,13 +4,13 @@ pipeline {
             image 'rust:latest'
         }
     }
-    stage('Rustfmt') {
-        steps {
-            // The build will fail if rustfmt thinks any changes are required.
-            sh "rustup component add rustfmt --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo fmt --all"
-        }
-    }
     stages {
+        stage('Rustfmt') {
+            steps {
+                // The build will fail if rustfmt thinks any changes are required.
+                sh "rustup component add rustfmt --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo fmt --all"
+            }
+        }
         stage('Build') {
             steps {
                 sh "cargo build"
