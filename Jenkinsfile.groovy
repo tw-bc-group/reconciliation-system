1485afce26520019258ec89df3ca1c1efc7a3689
赵正阳 <zhaozhengyang@zhaozhengyangdeMacBook-Pro.local>
2020-01-20 16:45:36 +0800

    (chore) comment clippy stage

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,11 +11,12 @@ pipeline {
                 sh "rustup component add rustfmt --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo fmt --all"
             }
         }
-        stage('Clippy') {
-            steps {
-                sh "rustup component add clippy --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo clippy --all"
-            }
-        }
+        // why comment this state: Running with more than 40 minutes on CI.
+        // stage('Clippy') {
+        //     steps {
+        //         sh "rustup component add clippy --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo clippy --all"
+        //     }
+        // }
         stage('Build') {
             steps {
                 sh "cargo build"
