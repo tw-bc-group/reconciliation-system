88ce9d5508eacfb90c2a3d27d3b58ab296a97935
赵正阳 <zhaozhengyang@zhaozhengyangdeMacBook-Pro.local>
2020-01-21 10:34:49 +0800

    (chore) test dockerize

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,35 +1,34 @@
 pipeline {
-    agent {
-        docker {
-            image "zzybing/rust-libclang:latest"
-        }
-    }
+    agent none
     stages {
         stage('Rustfmt') {
+            agent {
+                docker {
+                    image "zzybing/rust-libclang:latest"
+                }
+            }
             steps {
                 // The build will fail if rustfmt thinks any changes are required.
                 sh "rustup component add rustfmt --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo fmt --all"
             }
         }
-        // why comment this state: Running with more than 40 minutes on CI.
-        // stage('Clippy') {
+        // stage('Build and Test') {
+        //     agent {
+        //         docker {
+        //             image "zzybing/rust-libclang:latest"
+        //         }
+        //     }
         //     steps {
-        //         sh "rustup component add clippy --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo clippy --all"
+        //         sh "cargo build"
+        //         sh "cp ${env.WORKSPACE}/target/debug/*.so ${env.WORKSPACE}/reconciliation/tests/plugin/"
+        //         sh "cargo test"
         //     }
         // }
-        stage('Build') {
+        stage('Dockerize') {
+            agent any
             steps {
-                sh "cargo build"
-            }
-        }
-        stage("Move plugins to test folder") {
-            steps {
-                sh "cp ${env.WORKSPACE}/target/debug/*.so ${env.WORKSPACE}/reconciliation/tests/plugin/"
-            }
-        }
-        stage('Test') {
-            steps {
-                sh "cargo test"
+                sh 'make image'
+                sh 'make publish'
             }
         }
     }
