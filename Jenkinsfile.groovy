df2cc8b2ba712074a0f00a6f787c58d8a29fa6f0
赵正阳 <zhaozhengyang@zhaozhengyangdeMacBook-Pro.local>
2020-01-21 11:02:08 +0800

    (chore) add build and test stage

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,18 +12,18 @@ pipeline {
                 sh "rustup component add rustfmt --toolchain 1.40.0-x86_64-unknown-linux-gnu; cargo fmt --all"
             }
         }
-        // stage('Build and Test') {
-        //     agent {
-        //         docker {
-        //             image "zzybing/rust-libclang:latest"
-        //         }
-        //     }
-        //     steps {
-        //         sh "cargo build"
-        //         sh "cp ${env.WORKSPACE}/target/debug/*.so ${env.WORKSPACE}/reconciliation/tests/plugin/"
-        //         sh "cargo test"
-        //     }
-        // }
+        stage('Build and Test') {
+            agent {
+                docker {
+                    image "zzybing/rust-libclang:latest"
+                }
+            }
+            steps {
+                sh "cargo build"
+                sh "cp ${env.WORKSPACE}/target/debug/*.so ${env.WORKSPACE}/reconciliation/tests/plugin/"
+                sh "cargo test"
+            }
+        }
         stage('Dockerize') {
             agent any
             steps {
